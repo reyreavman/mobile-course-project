0cfb970d40efa7c79388c3d2a4cf1937cc8b606c
Radmir Khurum <radmirkhurum@gmail.com>
2023-05-06 22:49:03 +0700

    secCommit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,45 +1,24 @@
 package com.example.myapplication
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
+import android.widget.Button
 
 
-
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     val myTag = "myTag"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val button: Button = findViewById(R.id.signInButton)
+        button.setOnClickListener(this)
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.i(myTag, "onStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.i(myTag, "OnResume")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.i(myTag, "onRestart")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.i(myTag, "onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.i(myTag, "onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.i(myTag, "onDestroy")
+    override fun onClick(v: View?) {
+        val intent = Intent(this, MapActivity::class.java)
+        startActivity(intent)
     }
 
 }
