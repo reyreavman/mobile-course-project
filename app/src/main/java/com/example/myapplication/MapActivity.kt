4341fc78c90ed5d7394c978a7d9b1ad1b5f98ecd
Radmir Khurum <radmirkhurum@gmail.com>
2023-05-08 07:44:15 +0700

    thirdCommivVrode

---
+++ app/src/main/java/com/example/myapplication/MapActivity.kt
@@ -0,0 +1,78 @@
+package com.example.myapplication
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.tomtom.quantity.Distance
+import com.tomtom.sdk.location.LocationProvider
+import com.tomtom.sdk.location.android.AndroidLocationProvider
+import com.tomtom.sdk.location.android.AndroidLocationProviderConfig
+import com.tomtom.sdk.map.display.MapOptions
+import com.tomtom.sdk.map.display.TomTomMap
+import com.tomtom.sdk.map.display.camera.CameraOptions
+import com.tomtom.sdk.map.display.location.LocationMarkerOptions
+import com.tomtom.sdk.map.display.ui.MapFragment
+import kotlin.time.Duration.Companion.milliseconds
+
+class MapActivity : AppCompatActivity() {
+    val myTag = "myTag"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.map)
+        val checkingSelfPermissions = CheckingSelfPermissions(this)
+        val mapOptions = MapOptions(mapKey = "E5GJL8Q4LVnsok7GyIUKefysGlgqTlTg")
+        val mapFragment = MapFragment.newInstance(mapOptions)
+        supportFragmentManager.beginTransaction().replace(R.id.map_container, mapFragment).commit()
+        val androidLocationProviderConfig = AndroidLocationProviderConfig(
+            minTimeInterval = 100L.milliseconds,
+            minDistance = Distance.meters(3.0)
+        )
+        val locationProvider: LocationProvider = AndroidLocationProvider (
+            context = applicationContext,
+            config = androidLocationProviderConfig
+        )
+        locationProvider.enable()
+        val lastLocation = locationProvider.lastKnownLocation!!.position
+
+        mapFragment.getMapAsync { tomtomMap: TomTomMap ->
+            tomtomMap.setLocationProvider(locationProvider)
+            tomtomMap.enableLocationMarker(LocationMarkerOptions(type = LocationMarkerOptions.Type.Pointer))
+            tomtomMap.moveCamera(
+                CameraOptions(
+                    position = lastLocation,
+                    zoom = 12.0)
+            )
+
+        }
+        Log.i(myTag, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(myTag, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(myTag, "OnResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i(myTag, "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(myTag, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(myTag, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(myTag, "onDestroy")
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/ui/theme/Theme.kt
@@ -0,0 +1,12 @@
+package com.example.myapplication.ui.theme
+
+
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
