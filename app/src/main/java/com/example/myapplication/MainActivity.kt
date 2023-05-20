37a931fadac12fbc4432081dc4e634597babe8c5
Radmir Khurum <radmirkhurum@gmail.com>
2023-05-21 03:25:11 +0700

    fourthCommit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,25 +1,28 @@
 package com.example.myapplication
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent
 import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import com.example.myapplication.ui.theme.User
 
 class MainActivity : AppCompatActivity() {
-//    val myTag = "myTag"
-//    val loginTag = "loginTag"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val loginEditText: EditText = findViewById(R.id.editTextTextEmailAddress)
-        val passwordEditText: EditText = findViewById(R.id.editTextTextPassword)
+        setContentView(R.layout.login)
+
+        val loginEditText: EditText = findViewById(R.id.loginEditText)
+        val passwordEditText: EditText = findViewById(R.id.passwordEditText)
+        val registrationTextView: TextView = findViewById(R.id.toRegistrationTextView)
         var loginString: String
         var passwordString: String
+
         loginEditText.setOnKeyListener(object : View.OnKeyListener {
             override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
-                if (event.action == KeyEvent.ACTION_DOWN &&
-                    keyCode == KeyEvent.KEYCODE_ENTER
-                ) {
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                     loginString = loginEditText.text.toString()
                     return true
                 }
@@ -28,16 +31,17 @@ class MainActivity : AppCompatActivity() {
         })
         passwordEditText.setOnKeyListener(object : View.OnKeyListener {
             override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
-                if (event.action == KeyEvent.ACTION_DOWN &&
-                    keyCode == KeyEvent.KEYCODE_ENTER
-                ) {
+                if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                     passwordString = passwordEditText.text.toString()
                     return true
                 }
                 return false
             }
         })
-
+        registrationTextView.setOnClickListener{
+            val intent = Intent(this@MainActivity, RegistrationActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
 
